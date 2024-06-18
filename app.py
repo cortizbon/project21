7c32edd09afeb7c5b8340be6115e628f90adf46d
Carlos Ortiz <carlosortiz@192.168.0.3>
2024-06-17 20:48:03 -0500

    new plot

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.title("Test - App")
-X = np.random.normal(0, 10, size=(10000, 2))
+X = np.random.normal(0, 10, size=(10000, 3))
 
-fig, ax = plt.subplots(1, 2, figsize=(10, 6))
+fig, ax = plt.subplots(1, 3, figsize=(10, 6))
 
 ax[0].hist(X[::, 0], 
            color='red', 
@@ -21,4 +21,12 @@ ax[1].hist(X[::, 1],
 ax[1].spines['top'].set_visible(False)
 ax[1].spines['right'].set_visible(False)
 ax[1].set_title("X2", size=14)
+
+ax[2].hist(X[::, 2], 
+           color='blue', 
+           alpha=0.2, 
+           bins=50)
+ax[2].spines['top'].set_visible(False)
+ax[2].spines['right'].set_visible(False)
+ax[2].set_title("X2", size=14)
 st.pyplot(fig)
